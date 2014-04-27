8d668e7744460f1c08186be10cdca144dbd43477
n13i <n13i@m2hq.net>
2014-04-28 04:37:09 +0900

    メンバーリストからキャラデリ指示した場合に使用するセッションIDを指定するインターフェースを追加，送信先PIDを取得するインターフェースを追加，セッションの状態を強制的に帰還状態にできるようにした

--- exa/core/charadeli.h
+++ exa/core/charadeli.h
@@ -1,17 +1,17 @@
-extern string CHARADELI_MODULE_ID;
-
 extern int CHARADELI_STATUS_NOVISIT;
-extern int CHARADELI_STATUS_VISITING;
 extern int CHARADELI_STATUS_READY;
+extern int CHARADELI_STATUS_VISITING;
 
 void CharaDeli_init();
 
 int CharaDeli_newSession();
 void CharaDeli_destroySession(int sid);
+void CharaDeli_setDefaultSessionID(int sid);
 
 int CharaDeli_editMessage(int sid);
 
 void CharaDeli_setSendTo(int sid, string to);
+string CharaDeli_getSendTo(int sid);
 void CharaDeli_setSendDataName(int sid, string dataname);
 void CharaDeli_addSendMessageHeader(int sid, string key, string value);
 void CharaDeli_setSendMessageBody(int sid, string message);
@@ -23,5 +23,6 @@ int CharaDeli_doVisit(int sid);
 
 int CharaDeli_getStatus(int sid);
 boolean CharaDeli_isReturned(int sid);
+void CharaDeli_forceReturn(int sid);
 
 int CharaDeli_showReplyMessage(int sid);
