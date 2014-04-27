3aea38081b63a0f6303f5fa563391026e428f0b3
n13i <n13i@m2hq.net>
2014-04-28 04:41:42 +0900

    訪問時に代りのキャラに引き継ぐよう変更，訪問状態を外部から得られるようにインターフェースを追加，多段キャラデリのチェイン数をプロパティに記録するよう変更

--- exa/visit.h
+++ exa/visit.h
@@ -2,3 +2,7 @@ void Visit_init();
 void Visit_menu();
 void Visit_preference();
 void Visit_triggerAutoVisit();
+boolean Visit_isVisiting();
+int Visit_getCurrentChainCount();
+string Visit_getCurrentVisitingPID();
+void Visit_forceReturn();
