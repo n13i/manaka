047c572f31746b50b21a3bf738f284fbf1428895
n13i <n13i@m2hq.net>
2014-05-28 23:35:18 +0900

    キャラデリ時に現在ロードされているsurfaceで使用しているファイルを送るようにした

---
+++ exa/visit_common.h
@@ -0,0 +1,7 @@
+extern string VISIT_PROP;
+extern string VISIT_PROP_VISITING;
+extern string VISIT_PROP_CURRENTCHAIN;
+extern string VISIT_PROP_LASTCHAINPID;
+
+string Visit_generateVSTFile();
+string Visit_getSurfacePath();
